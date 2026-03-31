fe8344ded1bb53a81c60ed9419da95ee21e2a8af
Nandan Acharya <samarthachar18@gmail.com>
2026-03-31 23:41:11 +0530

    feat: add centralized API service

---
+++ client/src/services/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api',
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('studygen_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('studygen_token');
+      localStorage.removeItem('studygen_user');
+      window.location.href = '/auth';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
