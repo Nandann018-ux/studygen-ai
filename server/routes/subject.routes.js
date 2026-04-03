c7546a2f13d1bfdb5b4bdbcb73d7dd64c796e5c9
Nandan Acharya <samarthachar18@gmail.com>
2026-04-03 12:22:09 +0530

    fix(server/routes): standardize subject routes to pluralized /subjects endpoints

--- server/routes/subject.routes.js
+++ server/routes/subject.routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const { authenticateJWT } = require('../middleware/auth.middleware');
-const { addSubject, getSubjects } = require('../controllers/subject.controller');
+const { addSubject, getSubjects, updateSubject, deleteSubject } = require('../controllers/subject.controller');
+
 router.post('/', authenticateJWT, addSubject);
 router.get('/', authenticateJWT, getSubjects);
+router.put('/:id', authenticateJWT, updateSubject);
+router.delete('/:id', authenticateJWT, deleteSubject);
+
 module.exports = router;
